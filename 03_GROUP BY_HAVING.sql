4dd58fdd50f7fe00430dd1a6fe564d39a8d6e656
Jtaegyu <tgjeon1104@naver.com>
2024-02-29 17:49:35 +0900

    20240229 서브쿼리

--- 03_GROUP BY_HAVING.sql
+++ 03_GROUP BY_HAVING.sql
@@ -275,10 +275,85 @@ WHERE SALARY > 3000000;
 -- UNION ALL : 여러개의 쿼리 결과를 하나로 합치는 연산자
 -- UNION과의 차이점은 중복영역을 모두 포함시킨다. (합집합 +  교집합)
 
+-- 1) 부서코드가 'D5'
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+
+UNION ALL
+
+-- 2) 급여가 300만 초과
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
 
 
 -- MINUS : 선행 SELECT 결과에서 다음 SELECT 결과와 겹치는 부분을 제외한 나머지 부분만 추출(차집합)
 -- 부서 코드 D5 중 급여가 300만 초과인 직원 제외
+
+-- 1) 부서코드가 'D5'
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE DEPT_CODE = 'D5'
+
+MINUS
+
+-- 2) 급여가 300만 초과
+SELECT EMP_NAME, DEPT_CODE, SALARY
+FROM EMPLOYEE
+WHERE SALARY > 3000000;
+
+
+
+----------------------------------------------------------------------------
+
+
+-- 집합 연산자 실제 사용 예시
+
+-- * 집합 연산자 사용 시
+--   컬럼의 타입만 일치하면 연산 수행이 가능하다 *
+
+SELECT EMP_NAME, SALARY FROM EMPLOYEE
+WHERE EMP_ID = 200
+
+UNION 
+
+SELECT PHONE, 1000000 FROM EMPLOYEE
+WHERE EMP_ID = 201
+
+UNION 
+
+SELECT '홍길동', 4000000 FROM DUAL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- WORKBOOK-1번.sql
+++ WORKBOOK-1번.sql
@@ -100,6 +100,9 @@ ORDER BY ENTRANCE_DATE;
 -- 2번
 -- 춘 기술대학교의 교수 중 이름이 세 글자가 아닌 교수가 두 명 있다고 한다.
 -- 그 교수의 이름과 주민번호를 조회하는 SQL을 작성하시오.
+SELECT PROFESSOR_NAME , PROFESSOR_SSN
+FROM TB_PROFESSOR
+WHERE LENGTH(PROFESSOR_NAME) != 3;
 
 
 
@@ -109,31 +112,63 @@ ORDER BY ENTRANCE_DATE;
 -- 춘 기술대학교의 남자 교수들의 이름과 나이를 나이 오름차순으로 조회하시오.
 -- (단, 교수 중 2000년 이후 출생자는 없으며 출력 헤더는 "교수이름"으로 한다.
 -- 나이는 '만'으로 계산한다.)
-SELECT PROFESSOR_NAME 교수이름 , SUBSTR(PROFESSOR_SSN,1,6)
-       MONTH_BETWEEN(SYSDATE, TO_DATE(SUBSTR(PROFESSOR_SSN,1,6))) /12
-FROM TB_PROFESSOR
-WHERE SUBSTR(PROFESSOR_SSN,8,1) = '1';
 
 
+
+
+
+
+
+
+
+
+
+
+SELECT PROFESSOR_NAME 교수이름,
+	FLOOR(MONTHS_BETWEEN(SYSDATE, 
+								TO_DATE( 19 || SUBSTR(PROFESSOR_SSN, 1, 6) ) ) / 12) 나이
+FROM TB_PROFESSOR 
+WHERE SUBSTR(PROFESSOR_SSN, 8, 1) = '1'
+ORDER BY 나이 ASC;
 
 
 -- 4번
 -- 교수들의 이름 중 성을 제외한 이름만 조회하시오. 출력 헤더는 "이름"이 찍히도록 한다.
 -- (성이 2자인 경우의 교수는 없다고 가정)
+SELECT SUBSTR(PROFESSOR_NAME, 2) 이름
+FROM TB_PROFESSOR;
 
 
+-- 이름이 4글자인 경우도 생각했을 때의 SQL
+SELECT 
+	CASE
+		WHEN LENGTH(PROFESSOR_NAME) = 3
+		THEN SUBSTR(PROFESSOR_NAME, 2)
+		
+		WHEN LENGTH(PROFESSOR_NAME) = 4
+		THEN SUBSTR(PROFESSOR_NAME, 3)
+	END 이름
+
+FROM TB_PROFESSOR;
 
 
 
 -- 5번
 -- 춘 기술대학교의 재수생 입학자를 조회하시오.
 -- (19살에 입학하면 재수를 하지 않은 것!)
-
-
+SELECT STUDENT_NO , STUDENT_NAME 
+FROM TB_STUDENT
+WHERE EXTRACT(YEAR FROM ENTRANCE_DATE)
+			-   TO_NUMBER(19 || SUBSTR(STUDENT_SSN, 1, 2)) > 19;
+		
+		
 
 -- 6번
--- 춘 기술대학교의 2000년도 이후 입학자들은 학번이 A로 시작하게 되어있다.
--- 2000년도 이전 학번을 받은 학생들의 학번과 이름 조회하는 SQL을 작성하시오.
+-- 춘 기술대학교의 2000년도 이후 입학자들은 학번이 A로 시작하게 되어있다. 
+-- 2000년도 이전 학번을 받은 학생들의 학번과 이름 조회하는 SQL을 작성하시오
+SELECT STUDENT_NO, STUDENT_NAME
+FROM TB_STUDENT 
+WHERE STUDENT_NO NOT LIKE 'A%';
 
 
 
